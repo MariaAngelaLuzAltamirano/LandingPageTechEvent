a85b69c52af392830337dfe01ced8531158f6a03
Mauri Cucco <mscucco@hotmail.com>
2020-08-25 19:04:17 -0300

    Uniendo Administracion con el servidor

--- workshop_landing_tech/js/script-admin.js
+++ workshop_landing_tech/js/script-admin.js
@@ -2,47 +2,51 @@ let numeroAsisten = document.getElementById("numero-asisten");
 let numeroCancelados = document.getElementById("numero-cancelados");
 let numeroDisponibles = document.getElementById("numero-disponibles");
 
-let tableRows = document.getElementsByClassName("row-invitados");
+numeroDisponibles.textContent = 100; //capacidad de invitados disponible
+
+let newsBell = document.getElementById("newsBell");
 let secciones = document.querySelector(".secciones");
-let tablaInvitados = document.querySelector(".tabla-invitados.confirmados");
+let tablaInvitadosConfirmados = document.querySelector(".tabla-invitados.confirmados");
+let tablaInvitadosCancelados = document.querySelector(".tabla-invitados.cancelados");
 let cancelados = document.getElementById("cancelados");
 let invitados = document.getElementById("invitados");
 
 let captionInvitados = document.querySelector(".caption-invitados");
 
-numeroAsisten.textContent = tableRows.length;
-
-let eliminar = document.getElementsByClassName("fas fa-trash-alt");
-let contCancelados = 0;
+let contCancelados = 0; //inicializo la cantidad de cancelados
 
 document.addEventListener("click", () => {
 
     if(event.target.className == "fas fa-trash-alt") {
 
+        let eliminar = document.getElementsByClassName("fas fa-trash-alt");
+        let tableRowsConfirmados = document.getElementsByClassName("row-invitados-confirmados");
+        let firstName = document.getElementsByClassName("firstName");
+        let lastName = document.getElementsByClassName("lastName");
+        let mailtoConfirmado = document.getElementsByClassName("mailto-confirmado");
+
         for(let i = 0; i < eliminar.length; i++) {
 
             if(event.target === eliminar[i]) {
 
-                tableRows[i].remove();
+                crearInvitado(tablaInvitadosCancelados, firstName[i].textContent, lastName[i].textContent, mailtoConfirmado[i].textContent);
+
+                tableRowsConfirmados[i].remove();
+                
                 contCancelados += 1;
                 numeroCancelados.textContent = contCancelados;
-                numeroDisponibles.textContent = contCancelados;
+                numeroDisponibles.textContent = parseInt(numeroDisponibles.textContent) + 1;
 
                 if(eliminar.length == 0) {
 
                     secciones.remove();
-                    captionInvitados.textContent = "No hay invitados"
-                    captionInvitados.style.width = "20rem"
+                    captionInvitados.textContent = "No hay invitados confirmados"
+                    captionInvitados.style.color = "black";
+                    captionInvitados.style.width = "32rem";
                 }
                 return;
             }          
         }
-
-    }else if(event.target.className == "mailto") {
-
-        let mail = event.target.textContent;
-
-        event.target.setAttribute("href", `mailto:${mail}`);
 
     }else if(event.target.className == "numeros cancelados") {
 
@@ -68,6 +72,7 @@ $(document).ready(function() {
 });
 
 
+//LLAMO AL SERVIDOR PARA OBTENER LA LISTA DE INVITADOS
 
 async function getInvitados() {
     let endPoint = "http://localhost:3000/invitados";
@@ -75,8 +80,79 @@ async function getInvitados() {
     return resp;
 };
 
+let arrayInvitados; //array con los invitados
+
 getInvitados()
 .then(resp => resp.text())
-.then(data => console.log(data))
+.then(data => {
+
+    let response = JSON.parse(data);
+
+    arrayInvitados = response.data;
+
+    console.log(arrayInvitados);
+
+    numeroAsisten.textContent = arrayInvitados.length;
+
+    let invitadosNuevos = document.getElementById("invitados-nuevos");
+
+    invitadosNuevos.textContent = arrayInvitados.length;
+    newsBell.style.display = "flex";
+
+    numeroDisponibles.textContent = parseInt(numeroDisponibles.textContent) - parseInt(numeroAsisten.textContent);
+
+    for(let i = 0; i < arrayInvitados.length; i++) {
+        
+        crearInvitado(tablaInvitadosConfirmados, arrayInvitados[i].firstName, arrayInvitados[i].lastName, arrayInvitados[i].email);
+
+    }
+})
 .catch(error => console.log("ERROR", error));
+
+
+
+//FUNCIÓN PARA CREAR LAS FILAS DE INVITADOS 
+
+function crearInvitado(tabla, nombre, apellido, correo) {
+
+    let rowInvitado = document.createElement("tr");
+    let tdNombre = document.createElement("td");
+    let tdApellido = document.createElement("td");
+    let tdEmail = document.createElement("td");
+    let email = document.createElement("a");
+    let tdEliminar = document.createElement("td");
+    let iconEliminar = document.createElement("i");
+
+    tabla.appendChild(rowInvitado);
+
+    rowInvitado.appendChild(tdNombre);
+    rowInvitado.appendChild(tdApellido);
+    rowInvitado.appendChild(tdEmail);
+    
+    tdEmail.appendChild(email);
+
+    if(tabla == tablaInvitadosConfirmados) {
+
+        rowInvitado.appendChild(tdEliminar);
+        tdEliminar.appendChild(iconEliminar);
+        tdEliminar.className = "eliminar";
+        iconEliminar.className = "fas fa-trash-alt";
+        rowInvitado.className = "row-invitados-confirmados";
+        tdNombre.className = "firstName";
+        tdApellido.className = "lastName";
+        email.className = "mailto-confirmado";
+
+    }else {
+
+        rowInvitado.className = "row-invitados-cancelados";
+        email.className = "mailto-cancelado";
+    }
+
+    email.setAttribute("href", `mailto:${correo}`);
+    
+    tdNombre.textContent = nombre;
+    tdApellido.textContent = apellido;
+    email.textContent = correo;
+
+}
 
